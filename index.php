6dada6cc06d725191d6ce37b6bfadc48bbe4f566
He Jin <zeroluffy@foxmail.com>
2016-10-22 03:40:28 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"><!--Google font: lobster -->
+<link rel="stylesheet" href="bootstrap4/css/bootstrap.min.css">
+<script src="bootstrap4/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+<style>
+nav{
+  font-family: lobster;
+}
+body {
+  background-color: #f2e0e7;
+}
+</style>
+<div class="container">
+  <nav class="navbar navbar-fixed-top navbar-light bg-faded">
+    <a class="navbar-brand" href="#" style="font-size:25px;">Love <code>Who</code> U ARE &nbsp;<img src="img/index_img/love.png" title="Hearts" width="25"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" style="float:right;">
+
+    </button>  <div class="collapse" id="collapseExample"><br/><br/>
+    <div class="card card-block" style="background-color:#f8fbf6;">
+      <span>_________<a style="font-size:21px;" href="baidu.com">&nbsp;&nbsp;Plan&nbsp;&nbsp;</a>_____________</span><img src="img/index_img/poppy.png" style="width:18px;"><br/>
+      <span>_________<a style="font-size:21px;" href="baidu.com">&nbsp;&nbsp;Love Story&nbsp;&nbsp;</a>.____________</span><img src="img/index_img/carnation.png" style="width:17px;"><br/>
+      <span>_________<a style="font-size:21px;" href="baidu.com">&nbsp;&nbsp;Love Photos Album&nbsp;&nbsp;</a>._________</span><img src="img/index_img/daisy.png" style="width:16px;"><br/>
+      <span>_________<a style="font-size:21px;" href="baidu.com">&nbsp;&nbsp;Real-time Love Text Chat&nbsp;&nbsp;</a>._________</span><img src="img/index_img/lotus.png" style="width:15px;"><br/><!--class="text-muted": text in a line. not worked here -->
+    </div>
+  </nav>
+</div>
+<body><br/><br/><br/><br/>
+  <!--<center><img src="img/index_img/feifei.jpg" style="height:600px; width:auto;"></center>-->
+</body>
+<nav class="navbar navbar-fixed-bottom navbar-light bg-faded">
+  <a class="navbar-brand" href="#"><center>&nbsp;</center></a>
+</nav>
+</html>
